4e906510fdef768aa6a3f3b7c274d321d3f2ca36
Suraj Mugala <suraj.mugala@intimetec.com>
2023-04-16 17:39:40 +0530

    interfaces-type-assertions-switches

---
+++ interface_type/interface1.go
@@ -0,0 +1,25 @@
+package interfacetype
+
+/*
+Interface - In Go, an interface is a set of method signatures. When a type provides definition for all the methods in the interface,
+it is said to implement the interface.
+
+Empty interface - An interface that has zero methods is called an empty interface.
+- any type can be passed to empty interface
+*/
+
+/*
+Basic Implement of Interface :
+Example link : https://go.dev/play/p/OUSwVPWOGu0
+
+Interface with pointer receiver :
+Example link : https://go.dev/play/p/_lUwTs7WjrA
+
+Implementing multiple interfaces - A type can implement more than one interface
+
+Embedding interfaces - Although go does not offer inheritance, it is possible to create a new interfaces by embedding other interfaces.
+
+Zero value of Interface - . A nil interface has both its underlying value and as well as concrete type as nil.
+
+
+*/
---
+++ interface_type/type_assertions_switch.go
@@ -0,0 +1,8 @@
+package interfacetype
+
+/*Type assertion - is used to extract the underlying value of the interface.
+Example link : https://go.dev/play/p/kvDl-noouDt
+
+Type Switch - compare the concrete type of an interface against multiple types specified in various case statements.
+Example link : https://go.dev/play/p/9XtRxeTQXuz
+*/
